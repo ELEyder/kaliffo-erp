7f85c31ac69043825213c94bb8be0c60970792c4
PBMGC <dariogutierrez953@gmail.com>
2024-09-26 15:11:51 -0500

    funcion onchange colores final

--- src/Components/Tienda/TiendaContenido/Tablas/TablaProductos.jsx
+++ src/Components/Tienda/TiendaContenido/Tablas/TablaProductos.jsx
@@ -132,7 +132,7 @@ const TiendaProductos = ({ id,handlerefrescarSideCard1 }) => {
       rowKey={(record) => record.producto_id}
       ></Table>
       <TiendaAddProductos ModalProductoAddTiendaAbierto={ModalProductoAddTiendaAbierto}
-      closeModalProductoAddTiendaAbierto={closeModalProductoAddTiendaAbierto}/>
+      closeModalProductoAddTiendaAbierto={closeModalProductoAddTiendaAbierto} id={id}/>
     </>
   );
 };
