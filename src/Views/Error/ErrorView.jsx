86e4ae7d861959c396ad80ecac8cfa1b60b88d71
ELEyder <74992528+ELEyder@users.noreply.github.com>
2024-11-05 17:06:54 -0500

    temp style

--- src/Views/Error/ErrorView.jsx
+++ src/Views/Error/ErrorView.jsx
@@ -7,8 +7,8 @@ const ErrorView = () =>{
         <link rel="stylesheet" href="css/error/error.css" />
         <div className="content">
             <img src="/public/img/bg/error-3.gif" alt="" />
-            <h1>404</h1>
-            <h2>Página no encontrada</h2>
+            <h1><span>4</span>04</h1>
+            <h2><span>|</span> Página no encontrada</h2>
             <Link to={"trabajadores/tipo/ventas"}>Ir a la página principal</Link>
         </div>
         </>
