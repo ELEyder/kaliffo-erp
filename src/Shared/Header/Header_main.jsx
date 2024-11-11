4a97d0fbe38d990e8fbc7881586cc0c2ebce3ed4
ELEyder <74992528+ELEyder@users.noreply.github.com>
2024-11-11 13:27:08 -0500

    fix sidebar

--- src/Shared/Header/Header_main.jsx
+++ src/Shared/Header/Header_main.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { Button } from "antd";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons';
 import { Header } from "antd/es/layout/layout";
 import Title from "antd/es/typography/Title";
 import "@/assets/css/shared/header.css"
 
-const Header_main = () => {
+const Header_main = ({collapsed, setCollapsed}) => {
   return (
     <>
       <Header>
+      <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
         <Title>Kaliffo ERP</Title>
       </Header>
     </>
--- src/Shared/Sidebar/Sidebar.jsx
+++ src/Shared/Sidebar/Sidebar.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import { UserOutlined, RightOutlined, LeftOutlined } from '@ant-design/icons';
 import { Layout, Avatar, Typography, FloatButton } from 'antd';
-import Sidebar_menu from "./SidebarOptions";
+import SidebarOptions from "./SidebarOptions";
 import "@/assets/css/shared/sidebar.css"
 
 const { Sider } = Layout;
 const { Title } =Typography
 
 
-const Sidebar_main = () => {
+const Sidebar_main = ({collapsed}) => {
   return (
+
     <>
-    <Sider
+      <Sider trigger={null} collapsible collapsed={collapsed}
       width={250}
       breakpoint="lg"
       className="sidebar"
@@ -21,7 +22,7 @@ const Sidebar_main = () => {
         {/* <Avatar size={64} icon={<UserOutlined />} />
         <Title level={4} >Tienda 1</Title> */}
       </div>
-      <Sidebar_menu/>
+      <SidebarOptions/>
 
     </Sider>
 
