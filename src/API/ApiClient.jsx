e55930ee918eb6998d45aad0a2aca4e0cd41ae27
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-01-04 02:26:17 -0500

    ApiCliente Comentado

--- src/API/ApiClient.jsx
+++ src/API/ApiClient.jsx
@@ -1,21 +1,23 @@
 import axios from 'axios';
 
-//
+// Se crea una instancia de axios con configuraciones predeterminadas
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
-  timeout: 5000,
+  baseURL: 'http://localhost:3000', // URL base de la API
+  timeout: 5000, // Tiempo de espera para la solicitud
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json', // Tipo de contenido de la solicitud
   },
-  withCredentials: true,
+  withCredentials: true, // Habilita el envío de cookies en solicitudes CORS
 });
 
+// Se agrega un interceptor para modificar las solicitudes antes de que se envíen
 apiClient.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token'); // Se obtiene el token desde localStorage
   if (token) {
+    // Si hay un token, se agrega al encabezado Authorization
     config.headers.Authorization = `Bearer ${token}`;
   }
-  return config;
+  return config; // Devuelve la configuración modificada
 });
 
-export default apiClient;
+export default apiClient; // Exporta la instancia para su uso en otras partes de la aplicación
