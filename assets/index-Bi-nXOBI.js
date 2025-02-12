3e7b75cf16f6f960af57bdb2c295d35ff8ba3a4f
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-11 22:39:49 -0500

    Updates

--- assets/index-Bi-nXOBI.js
+++
@@ -1,1 +0,0 @@
-import{d as i,j as o}from"./index-BmWU6D_k.js";import{S as r}from"./index-NOsMt7kP.js";import{B as n}from"./button-FicSlTbO.js";import"./useSize-C8agW3TM.js";import"./gapSize-U1swVQyS.js";const c=()=>{const t=i();return o.jsx(o.Fragment,{children:o.jsx(r,{children:o.jsx(n,{type:"primary",onClick:()=>t("pong"),children:"Ping"})})})};export{c as default};
