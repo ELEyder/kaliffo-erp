41fc28d41363a812b64dbed9d2e79e777e1374ae
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-02-25 12:28:46 -0500

    add: Reportes de tiendas

--- src/API/administrativo/Reporte.jsx
+++ src/API/administrativo/Reporte.jsx
@@ -15,7 +15,7 @@ export const getReporteUsuario = async (id,tipo) => {
 // Obtener reporte de una tienda http://localhost:3000/tienda/reporte/1
 export const getReporteTienda = async (id) => {
   try {
-    const response = await apiClient.get(`/tienda/reporte/${id}`, { responseType: 'blob' });
+    const response = await apiClientFiles.get(`/tienda/reporte/${id}`, { responseType: 'blob' });
 
     const pdf = response.data;
     const url = window.URL.createObjectURL(pdf);
