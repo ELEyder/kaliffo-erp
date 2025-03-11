93627f4fd0a478708a0a77eee86ab7b3e054bd14
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-03-11 03:04:04 -0500

    deploy code

--- src/modules/logistico/components/Cards/index.jsx
+++ src/modules/logistico/components/Cards/index.jsx
@@ -1,2 +1,4 @@
 export { default as AlmacenCard } from "./AlmacenCard";
 export { default as MovimientosTiposCards } from "./MovimientosTiposCards";
+export { default as MovimientoAlmacenTiendaCard } from "./MovimientoAlmacenTiendaCard";
+export { default as MovimientoTiendaTiendaCard } from "./MovimientoTiendaTiendaCard";
